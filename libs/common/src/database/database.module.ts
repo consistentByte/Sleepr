188a27a5929e09f4594b99fd7d202a8389af69af
Saurabh Pandey <consistentByte@gmail.com>
2026-02-08 08:36:24 +0530

    2-1 => Database and Config Module.
    forRootAsync,
    ConfigModule,
    Mongoose

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '../config/config.module';
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
